d15a6c1beeece940c7c496bf2b59f1e9bc6caf48
Your Name <your.email@example.com>
2025-03-27 01:18:47 -0400

    node 2

--- dashboard/static/js/gpus.js
+++ dashboard/static/js/gpus.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     let gpuData = [];
     const urls = [
         "http://128.151.20.95:5000",
+        "http://128.151.20.120:5000"
     ];
 
     function renderGPUNodes() {
